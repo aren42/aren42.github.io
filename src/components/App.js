d489f3e96dedb5ec1ef4ec45d5404bc3d124aa1d
vmariot_ <vmariot_@Victors-MacBook-Pro.local>
2017-06-19 15:51:56 +0200

    Clean

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Interactive from 'react-interactive';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
-import Navbar from './NavbarComponent';
+import Navbar from './Header';
+import PageNotFound from './PageNotFound';
 import s from '../styles/app.style';
 
 export default function App() {
   return (
     <div style={s.root}>
       <nav style={s.breadcrumbs}>
-        <Navbar />
+        
       </nav>
 
       <Switch>
